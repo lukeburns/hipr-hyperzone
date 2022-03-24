2cc4f55a87f90641daf6068936d765b7ee4320fb
Luke Burns <lukemburns@gmail.com>
2022-03-23 22:54:43 -0700

    update deps

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
-const HOME = require('os').homedir()
+const HOME = require('os').homedir();
 
-const base32 = require('bs32')
-const { Zone, wire, dnssec } = require('bns')
-const { SOARecord, Record, codes, types } = wire
-const Replicator = require('@hyperswarm/replicator')
-const Hyperzone = require('hyperzone')
+const base32 = require('bs32');
+const { Zone, wire, dnssec } = require('bns');
+const { SOARecord, Record, codes, types } = wire;
+const Replicator = require('@hyperswarm/replicator');
+const Hyperzone = require('hyperzone');
 
-const empty = new Zone()
+const empty = new Zone();
 
 // todo: Class-ify
 function middleware (dir) {
-  const zones = new Map()
-  const storageDir = dir || `${HOME}/.hyperzones/auth`
-  const hyperzoneOpts = { sparse: true, alwaysUpdate: true }
-  const replicatorOpts = { client: true, server: true }
-  const replicator = new Replicator()
-  replicator.on('connection', () => console.log('[hyperzone] connection')) // todo: fix leaks
+  const zones = new Map();
+  const storageDir = dir || `${HOME}/.hyperzones/auth`;
+  const hyperzoneOpts = { sparse: true, alwaysUpdate: true };
+  const replicatorOpts = { client: true, server: true };
+  const replicator = new Replicator();
+  replicator.on('connection', () => console.log('[hyperzone] connection')); // todo: fix leaks
 
   const add = (key, opts) => {
-    const buf = base32.decode(key)
-    key = key.toString('hex')
-    let zone = zones.get(key)
+    const buf = base32.decode(key);
+    key = key.toString('hex');
+    let zone = zones.get(key);
     if (zone) {
-      return zone
+      return zone;
     }
 
     const promise = new Promise(async (resolve) => {
-      const storage = `${storageDir}/${key}`
-      zone = new Hyperzone(storage, key, opts)
-      zones.set(key, zone)
-      resolve(zone)
-      replicator.add(zone.db, replicatorOpts)
-      await zone.ready()
-      const origin = await zone.origin()
-      zones.set(origin, zone)
-    })
+      const storage = `${storageDir}/${key}`;
+      zone = new Hyperzone(storage, key, opts);
+      zones.set(key, zone);
+      resolve(zone);
+      replicator.add(zone.db, replicatorOpts);
+      await zone.ready();
+      const origin = await zone.origin();
+      zones.set(origin, zone);
+    });
 
-    zones.set(key, promise)
+    zones.set(key, promise);
 
-    return promise
-  }
+    return promise;
+  };
 
   return {
-    hostname: ':data.:protocol(_hyper|_hyperzone|hyperzone|ns.direct).:gateway?.', 
+    hostname: ':data.:protocol(_hyperzone|hyperzone).:gateway?.',
     handler: async ({ protocol, data }, name, type, response) => {
       if (name.indexOf(protocol) > 0) {
-        return null
+        return null;
       }
 
       for (const [origin, zone] of zones.entries()) {
-        const s = name.split(origin)
+        const s = name.split(origin);
         if (!s[s.length - 1]) {
-          return await zone.resolve(name, type)
+          return await zone.resolve(name, type);
         }
       }
 
-      data = data.split('.')
-      const key = data[data.length - 1]
+      data = data.split('.');
+      const key = data[data.length - 1];
       if (key.length === 52) {
-        const zone = await add(key)
+        const zone = await add(key);
         if (zone.origin) {
-          const origin = await zone.origin()
+          const origin = await zone.origin();
           if (origin) {
-            return await zone.resolve(name, type)
+            return await zone.resolve(name, type);
           }
         }
-        const res = empty.resolve(name, types[type])
-        res.code = codes.SERVFAIL // ensure response not cached
-        return res
+        const res = empty.resolve(name, types[type]);
+        res.code = codes.SERVFAIL; // ensure response not cached
+        return res;
       } else {
-        return response
+        return response;
       }
     }
-  }
+  };
 }
 
-module.exports = middleware
+module.exports = middleware;
